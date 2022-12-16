d9e1b55926497eb08932986983111268624c915f
yjcyxky <yjcyxky@163.com>
2022-12-16 13:09:11 +0800

    Improve styles for about page.

--- studio/src/pages/About/index.tsx
+++ studio/src/pages/About/index.tsx
@@ -6,7 +6,7 @@ import { getDownload as getFile } from '@/services/swagger/File';
 const About: React.FC = () => {
   const markdownLink = '/README/about.md'
 
-  return <Card className="about" style={{ width: '100%', height: '100%' }}>
+  return <Card className="about" style={{ width: '100%', height: '100%', overflow: 'scroll' }}>
     <MarkdownViewer getFile={getFile} url={markdownLink} />
   </Card>;
 };
