7c890a84d9c75b4abccdef238fddb62119038a38
yjcyxky <yjcyxky@163.com>
2023-10-08 01:01:10 -0500

    [Fix Bug] Cannot get correct token from cookie.

--- src/app.tsx
+++ src/app.tsx
@@ -34,13 +34,10 @@ const getJwtAccessToken = (): string | null => {
   if (document.cookie && document.cookie.includes("jwt_access_token=")) {
     // Retrieve the cookie value
     // @ts-ignore
-    const cookieValue = document.cookie
+    jwtToken = document.cookie
       .split("; ")
       .find((row) => row.startsWith("jwt_access_token="))
       .split("=")[1];
-
-    // Parse the JWT token
-    jwtToken = JSON.parse(atob(cookieValue.split(".")[1]));  
   }
 
   if (jwtToken) {
